f0d0215b61592415df6a5c0fe8d5ec3486e885ed
chetan <alexantar96@gmail.com>
2025-12-07 16:06:09 +0530

    new images

--- src/seed/product.js
+++ src/seed/product.js
@@ -4,28 +4,32 @@ export const products = [
     name: "Coca Cola",
     description: "Chilled fizzy cola drink",
     price: 49,
-    image: "https://picsum.photos/300/200?drink2",
+    image:
+      "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?auto=format&fit=crop&w=600&q=80",
     category: "drinks",
   },
   {
     name: "Pepsi",
     description: "Refreshing cola beverage",
     price: 49,
-    image: "https://picsum.photos/300/200?drink3",
+    image:
+      "https://images.unsplash.com/photo-1629203851122-3726ecdf080e?auto=format&fit=crop&w=600&q=80",
     category: "drinks",
   },
   {
     name: "Lemon Iced Tea",
     description: "Refreshing lemon-flavored iced tea",
     price: 79,
-    image: "https://picsum.photos/300/200?drink4",
+    image:
+      "https://images.unsplash.com/photo-1513558161293-cdaf765ed2fd?auto=format&fit=crop&w=600&q=80",
     category: "drinks",
   },
   {
     name: "Mango Shake",
     description: "Thick and sweet mango shake",
     price: 129,
-    image: "https://picsum.photos/300/200?drink5",
+    image:
+      "https://images.unsplash.com/photo-1546173159-315724a31696?auto=format&fit=crop&w=600&q=80",
     category: "drinks",
   },
 
@@ -34,21 +38,25 @@ export const products = [
     name: "Cheese Burger",
     description: "Juicy patty with melted cheese",
     price: 149,
-    image: "https://picsum.photos/300/200?burger1",
+    image:
+      "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?auto=format&fit=crop&w=600&q=80",
     category: "burger",
   },
   {
     name: "Chicken Burger",
     description: "Crispy chicken patty burger",
     price: 179,
-    image: "https://picsum.photos/300/200?burger2",
+    image:
+      "https://images.unsplash.com/photo-1615297348928-8319f7584141?auto=format&fit=crop&w=600&q=80",
     category: "burger",
   },
   {
     name: "Paneer Burger",
     description: "Soft paneer patty with spicy sauce",
     price: 159,
-    image: "https://picsum.photos/300/200?burger3",
+    // Using a high-quality veggie burger image that resembles a paneer patty
+    image:
+      "https://images.unsplash.com/photo-1520072959219-c595dc870360?auto=format&fit=crop&w=600&q=80",
     category: "burger",
   },
 
@@ -57,21 +65,24 @@ export const products = [
     name: "Veg Sandwich",
     description: "Loaded with fresh veggies",
     price: 99,
-    image: "https://picsum.photos/300/200?sandwich1",
+    image:
+      "https://images.unsplash.com/photo-1509722747741-092071b83ea2?auto=format&fit=crop&w=600&q=80",
     category: "sandwich",
   },
   {
     name: "Grilled Cheese Sandwich",
     description: "Cheesy and perfectly grilled",
     price: 119,
-    image: "https://picsum.photos/300/200?sandwich2",
+    image:
+      "https://images.unsplash.com/photo-1528736235302-52922dfa4551?auto=format&fit=crop&w=600&q=80",
     category: "sandwich",
   },
   {
     name: "Chicken Sandwich",
     description: "Grilled chicken with mayo",
     price: 149,
-    image: "https://picsum.photos/300/200?sandwich3",
+    image:
+      "https://images.unsplash.com/photo-1606754752353-b2f8d3d61d86?auto=format&fit=crop&w=600&q=80",
     category: "sandwich",
   },
 ];
